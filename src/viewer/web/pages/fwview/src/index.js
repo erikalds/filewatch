1095276198feadf110ea461076e81c7f04ee2080
erikalds <erikalds@gmail.com>
2020-05-25 08:40:03 +0200

    [WIP] Some work on the UI

--- src/viewer/web/pages/fwview/src/index.js
+++ src/viewer/web/pages/fwview/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TreeMenu from 'react-simple-tree-menu';
+import TreeMenu, { ItemComponent } from 'react-simple-tree-menu';
 import './index.css';
+
+class FileContainerView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="FileContainerview">
+            </div>
+        );
+    }
+}
 
 class Filelist extends React.Component {
     constructor(props) {
@@ -24,20 +36,91 @@ class Filelist extends React.Component {
     render() {
         return (
             <div className="Filelist">
-                <TreeMenu data={this.state.treeData} />
+                <TreeMenu
+                    data={this.state.treeData}
+                    onClickItem={({ key, label, ...props }) => {
+                        this.props.viewFile(key);
+                    }}
+                >
+                {({ search, items }) => (
+                    <ul>
+                        { items.map(({key, ...props}) => (
+                            <ItemComponent
+                                key={key}
+                                label={this.createLabel(props)}
+                                {...this.restOfProps(props)} />
+                        ))}
+                    </ul>
+                )}
+                </TreeMenu>
             </div>
         );
+    }
+    createLabel(props) {
+        let label = props.label + ", ";
+        if (typeof props.size !== 'undefined') {
+            label += props.size + " B, ";
+        }
+        else {
+            label += " --, ";
+        }
+        var time = new Date(props.mtime);
+        label += time.toISOString();
+        return label;
+    }
+    restOfProps(props) {
+        delete props.label;
+        return props;
     }
 }
 
 class FilewatcherView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [],
+            activeFile: null,
+        };
+    }
     render() {
+        console.log("FilewatcherView render state: ", this.state);
         return (
             <div className="FilewatcherView">
                 <h1>Filewatcher</h1>
-                <Filelist/>
+                <Filelist
+                    viewFile={(key) => this.viewFile(key)}
+                />
+                <FileContainerView
+                    files={this.state.files}
+                    activeFile={this.state.activeFile}
+                    setActiveFile={(key) => this.viewFile(key)}
+                    closeFile={(key) => this.closeFile(key)}
+                />
             </div>
         );
+    }
+    viewFile(key) {
+        var files = this.state.files.slice();
+        if (!files.includes(key)) {
+            files.push(key);
+        }
+        this.setState({
+            files: files,
+            activeFile: key,
+        });
+    }
+    closeFile(key) {
+        var files = this.state.files.slice();
+        if (!files.includes(key)) {
+            return;
+        }
+        var idx = files.indexOf(key);
+        files = files.slice(0, idx).concat(files.slice(idx + 1));
+        this.setState({
+            files: files,
+            activeFile: files[Math.max(0, idx - 1)],
+        });
+
     }
 }
 
